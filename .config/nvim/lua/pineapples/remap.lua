471fadc0a58273e9c1324af6bd8fc1532b5662e3
du-cki <qwuakers@gmail.com>
2024-03-10 20:20:06 +0400

    Reduce boiler.

--- .config/nvim/lua/pineapples/remap.lua
+++ .config/nvim/lua/pineapples/remap.lua
@@ -1,37 +1,38 @@
-
 -- supperior leader key
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
+local km = vim.keymap
 
-vim.keymap.set("n", "<A-w>", "yy")
-vim.keymap.set({"x", "o"}, "<A-w>", "y")
+km.set("n", "<leader>fd", vim.cmd.Ex)
 
-vim.keymap.set("n", "<Space>so", ":so<CR>:PackerSync<CR>")
+km.set("n", "<A-w>", "yy")
+km.set({"x", "o"}, "<A-w>", "y")
 
-vim.keymap.set("n", "<C-_>", "u")
+km.set("n", "<Space>so", ":so<CR>:PackerSync<CR>")
+
+km.set("n", "<C-_>", "u")
 
 -- Additional Window utility
-vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split horizontally
+km.set("n", "<leader>sv", "<C-w>v") -- split vertically
+km.set("n", "<leader>sh", "<C-w>s") -- split horizontally
 
-vim.keymap.set("n", "<leader>sd", "<C-w>c") -- close split window
+km.set("n", "<leader>sd", "<C-w>c") -- close split window
 
 -- moves selected line (in visual mode) up or down (and format it)
-vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
+km.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")
+km.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
 
 -- moves line (in normal mode) up or down (and format it)
-vim.keymap.set("n", "<M-Up>", ":m -2<CR>v=")
-vim.keymap.set("n", "<M-Down>", ":m +1<CR>v=")
+km.set("n", "<M-Up>", ":m -2<CR>v=")
+km.set("n", "<M-Down>", ":m +1<CR>v=")
 
 -- copy to register a and paste above or below current line 
-vim.keymap.set("x", "<M-S-Up>", ":yank a<CR>\"aPV")
-vim.keymap.set("x", "<M-S-Down>", ":yank a<CR>\"apV")
+km.set("x", "<M-S-Up>", ":yank a<CR>\"aPV")
+km.set("x", "<M-S-Down>", ":yank a<CR>\"apV")
  
 -- center screen 
-vim.keymap.set({"n", "v", "x"}, "<C-l>", "zz")
+km.set({"n", "v", "x"}, "<C-l>", "zz")
 
 -- indent/dedent through tabs
-vim.keymap.set("x", "<Tab>", ">gv")
-vim.keymap.set("x", "<S-Tab>", "<gv")
+km.set("x", "<Tab>", ">gv")
+km.set("x", "<S-Tab>", "<gv")
